057f40120f7752224476cc88d3e2909aa8e6a309
Muhammad Fawwaz EFS <faw.edsa@gmail.com>
2024-11-13 15:40:41 +0700

    menyelesaikan tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
+import json
 from django.shortcuts import render, redirect, reverse  # Tambahkan import redirect di baris ini
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
-from django.http import HttpResponse,  HttpResponseRedirect
+from django.http import HttpResponse,  HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
@@ -15,6 +16,7 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -130,3 +132,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
